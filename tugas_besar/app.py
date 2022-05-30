89327a442202d8700f1379fe0b76712f1b4af0ea
unknown <mohamadburhan151@gmail.com>
2022-05-30 08:19:33 +0700

    Menambahkan Operasi CRUD

--- tugas_besar/app.py
+++ tugas_besar/app.py
@@ -2,15 +2,21 @@ from itertools import count
 from turtle import title
 from flask import Flask, render_template, session, \
 	request, redirect, url_for
-from models import Pengguna
+from models import Pengguna, Database
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '1234567890'
-
+app.config['UPLOAD_FOLDER'] = os.path.realpath('.') + '/tugas_besar/static/uploads'
+app.config['MAX_COTENT_PATH'] = 10000000
 
 
 @app.route('/')
 def index():
+	g = []
+	gmbr = Database()
+	g = gmbr.selectAll('landingPage')
 	if 'username' in session:
 		username = session['username']
 		password = session['password']
@@ -24,7 +30,7 @@ def index():
 			elif kolom[5] == 'admin':
 				return render_template('index.admin.html', tabel=tabel, username=username, title="Index Admin"
 			)
-	return render_template('landingPage.html', title="Landing Page")
+	return render_template('landingPage.html', title="Landing Page", gmbrs=g)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -71,8 +77,104 @@ def varData():
 		return render_template('varTData.html',
 			username=username, title="Variabel Dan Tipe Data"
 		)
-	return render_template('landingPage.html', title="Landing Page")
+	return redirect(url_for('logout'))
 
+@app.route('/tblUser', methods=['GET', 'POST'])
+def tblUser():
+	if 'username' in session:
+		username = session['username']
+
+		dataUser = []
+		users = Database()
+		dataUser = users.selectAll('user')
+		if request.method == 'POST':
+			data = {
+				'nama' : request.form['nama'],
+				'username' : request.form['username'],
+				'password' : request.form['password'],
+				'level' : request.form['level'],
+				'aksi' : request.form['aksi']
+			}
+			if data['aksi'] == 'tambah':
+				tambah = Database()
+				tambah.act(data)
+				dataUser=[]
+				dataUser=tambah.selectAll('user')
+				return render_template('tblUser.html',
+					username=username, title="Tabel User", data=dataUser
+				)
+			elif data['aksi'] == 'edit':
+				dataEdit = {
+					'nama' : request.form['nama'],
+					'username' : request.form['username'],
+					'password' : request.form['password'],
+					'level' : request.form['level'],
+					'aksi' : request.form['aksi'],
+					'id' : int(request.form['id'])
+				}
+				edit = Database()
+				edit.act(dataEdit)
+				datas = []
+				datas= edit.selectAll('user')
+				return render_template('tblUser.html',
+					username=username, title="Tabel User", data=datas
+				)
+		return render_template('tblUser.html',
+			username=username, title="Tabel User", data=dataUser
+		)
+	return redirect(url_for('logout'))
+
+@app.route('/editUser/<id>', methods=['GET', 'POST'])
+def editUser(id):
+	if 'username' in session:
+		username = session['username']
+		if request.method == 'GET':
+			data = []
+			users = Database()
+			data = users.selectAll('user')
+			dt=[]
+			dt = users.select('user', 'idUser', id)
+			return render_template('editUser.html',
+				username=username, title="Tabel User", data=data, dt=dt
+			)
+	return redirect(url_for('logout'))
+
+@app.route('/tblUserDel/<id>', methods=['GET', 'POST'])
+def tblUserDel(id):
+	if 'username' in session:
+		if request.method == 'GET':
+			hapus = Database()
+			hapus.delete('user', 'idUser', id)
+			return redirect(url_for('tblUser'))
+			
+	return redirect(url_for('logout'))
+
+@app.route('/tblLP', methods=['GET', 'POST'])
+def tblLP():
+	if 'username' in session:
+		username = session['username']
+
+		dataUser = []
+		users = Database()
+		dataUser = users.selectAll('landingPage')
+		if request.method == 'POST':
+			if request.form['aksi'] == 'tambah':
+				f = request.files['gambar']
+				filename = app.config['UPLOAD_FOLDER'] + '/' + secure_filename(f.filename)
+				f.save(filename)
+				data = {
+						'judul' : request.form['judul'],
+						'deskripsi' : request.form['deskripsi'],
+						'gambar' : f.filename,
+						'aksi' : request.form['aksi']
+					}
+				tambah = Database()
+				tambah.tambahLP(data)
+			return redirect(url_for('tblLP'))
+		return render_template('tblLandingPage.html',
+			username=username, title="Tabel Landing Page", data=dataUser
+		)
+	return redirect(url_for('logout'))
 	
 if __name__ == '__main__':
 	app.run(debug=True)
--- tugas_besar/models.py
+++ tugas_besar/models.py
@@ -46,6 +46,64 @@ class Pengguna():
 		self.closeDB()
 		return True if count_account > 0 else False	
 			
-			
-			
-			
+class Database():
+	
+	def openDB(self):
+		global db, cursor
+		db = pymysql.connect(
+			host = config.DB_HOST,
+			user = config.DB_USER,
+			password = config.DB_PASSWORD,
+			database = config.DB_NAME)
+		cursor = db.cursor()
+	
+	def closeDB(self):
+		global db, cursor
+		db.close()
+	
+	def selectAll(self, namaTable):
+		self.tbl = namaTable
+		self.openDB()
+		cursor.execute("SELECT * FROM %s" % self.tbl)
+		container = []
+		for self.col in cursor.fetchall():
+			container.append((self.col))
+		self.closeDB()
+		return container
+	
+	def select(self, namaTable,kol,  id):
+		self.tbl = namaTable
+		self.openDB()
+		cursor.execute("SELECT * FROM %s WHERE %s = %s" % (self.tbl, kol, id))
+		container = []
+		for kol in cursor.fetchall():
+			container.append(kol)
+		self.closeDB()
+		return container
+
+	def act(self,data):
+		
+		self.data = data
+		if self.data['aksi'] == 'tambah':
+			self.openDB()
+			cursor.execute("INSERT INTO user(namaUser, userName, password, keterangan, level) VALUES('%s' , '%s' , MD5('%s') , '%s' , '%s')"  % (self.data['nama'], self.data['username'], self.data['password'], self.data['password'], self.data['level']))
+			db.commit()
+			self.closeDB()
+		if self.data['aksi'] == 'edit':
+			self.openDB()
+			cursor.execute("UPDATE user SET namaUser='%s', userName='%s', password=MD5('%s'), keterangan='%s', level='%s' WHERE idUser=%d"  % (self.data['nama'], self.data['username'], self.data['password'], self.data['password'], self.data['level'], self.data['id']))
+			db.commit()
+			self.closeDB()
+	def delete(self,tabel, kol, id):
+		self.openDB()
+		cursor.execute("DELETE FROM %s WHERE %s = %s" % (tabel, kol, id))
+		db.commit()
+		self.closeDB()
+	def tambahLP(self, data):
+		self.data = data
+		if self.data['aksi'] == 'tambah':
+			self.openDB()
+			cursor.execute("INSERT INTO landingPage(judul, deskripsi, gambar) VALUES('%s' , '%s', '%s')"  % (self.data['judul'], self.data['deskripsi'], self.data['gambar']))
+			db.commit()
+			self.closeDB()
+
